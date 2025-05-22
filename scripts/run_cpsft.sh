d4809942829e018c5f2f4dd02f023943bf76dcaa
Kim Sooyeon <kksooyeon@gmail.com>
2025-05-22 09:24:19 +0900

    minor fix

--- scripts/run_cpsft.sh
+++ scripts/run_cpsft.sh
@@ -5,13 +5,13 @@ pip install deepspeed
 pip install peft
 pip install -U transformers
 
-deepspeed --num_gpus=8 ./src/CPSFT/cpsft/train_sft.py\
- --data_path sft.json \
+deepspeed --num_gpus=1 ./src/CPSFT/cpsft/train_sft.py\
+ --data_path ./H2_sft.json \
  --deepspeed ./src/CPSFT/cpsft/deepspeed_config/ZeRO_3.json \
- --output_dir /data/checkpoints/mistral_sft/ \
+ --output_dir ./data/checkpoints/mistral_sft/ \
  --eval_steps 20 \
  --save_steps 100 \
- --base_model /mistralai/Mistral-7B-Instruct-v0.2 \
+ --base_model mistralai/Mistral-7B-Instruct-v0.2 \
  --prompt_template_name mistral_delete
 
 
